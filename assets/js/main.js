f5ea7e5a6f08c9cd3e143550fa380afaa0519938
omarghandour <omarkhaled9913@gmail.com>
2024-09-06 00:00:44 +0300

    third  commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,19 +184,3 @@ $(document).ready(function () {
     $(this).toggleClass("active");
   });
 });
-
-const ll = document.querySelector(".hiddenBurger");
-let i = 0;
-document.querySelector(".hiddenBurger").onclick = () => {
-  document.querySelector(".hiddenBurger").classList.toggle("rotate");
-  if (i === 0) {
-    document.getElementById("board").classList.remove("hidden");
-    document.getElementById("board").classList.toggle("flex");
-    i = 1;
-  } else {
-    document.getElementById("board").classList.toggle("hidden");
-    document.getElementById("board").classList.remove("flex");
-
-    i = 0;
-  }
-};
